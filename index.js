24e77096115e6170bc27804b635c9ca2f1451b9d
Egg Sandwich <eggsandwich@Eggs-MacBook-Pro.local>
2019-02-05 13:13:01 -0700

    add buttons

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
-function onLoadContent() {
-  console.log("loaded");
-}
+document.addEventListener('DOMContentLoaded', onLoadContent);
 
 function onClickYay() {
   console.log("clicked");
 }
 
-document
-  .getElementById('btn-yay')
-  .addEventListener('click', onClickYay());
+function onLoadContent() {
 
-document.addEventListener('DOMContentLoaded', onLoadContent());
+    //this button shoud get value from input yay-input
+    //and when we press the button we should add a new li to our things-list
+  document
+    .getElementById('yay-input')
+    .addEventListener('change', function () {
+      let newItem = document.getElementById('yay-input'.input;
+      console.log("new list item: ", newItem);
+    })
+}
 
-// let arr = ["thing", 2, 5.0, [1, 2, 4]];
-//
-// for (var i = 0; i < arr.length; i++) {
-//   arr[i]
-// }
+function keyInputAddItem () {
+  console.log("change input value: ", this.value);
+
+}
+
+
+let arr = ["thing", 2, 5.0, "string2", "hello", 5, 6];
+
+let numbers = [];
+
+let content =[];
+
+for (var i = 0; i < arr.length; i++) {
+  if(typeof arr[i] === 'string'){
+    content.push(arr[i]);
+  } else if (typeof arr[i] === 'number'){
+    numbers.push(arr[i]);
+  }
+
+}
+
+
+
+var ulList = document.getElementById('things-list')
+
+for (var i = 0; i < content.length; i++) {
+  let listItem = document.createElement('li');
+  listItem.textContent = content[i];
+  ulList.append(listItem);
+}
